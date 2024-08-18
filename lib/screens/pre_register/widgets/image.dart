843486f33ddcb6c66a1851425a0f81b041f57f34
Neways3 <neways3snit@gmail.com>
2024-08-18 12:14:57 +0600

    front camera open directly done

--- lib/screens/pre_register/widgets/image.dart
+++ lib/screens/pre_register/widgets/image.dart
@@ -1,60 +1,17 @@
-// ignore_for_file: must_be_immutable
-
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:super_home_member/config/constanent.dart';
 import 'package:super_home_member/screens/pre_register/controller/PreRegisterController.dart';
-import 'package:super_home_member/screens/request/complain/bloc/ComplainController.dart';
 
-import '../../widgets/constants.dart';
+import '../../../config/constanent.dart';
 
 class ImageUpload extends StatelessWidget {
   PreRegisterController controller =
-      Get.put<PreRegisterController>(PreRegisterController());
+  Get.put<PreRegisterController>(PreRegisterController());
 
   ImageUpload({Key? key}) : super(key: key);
-
-  //========================= Gellary / Camera AlerBox
-  void _onAlertPress(context) async {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return CupertinoAlertDialog(
-            actions: [
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/gallery.png',
-                      width: 50,
-                    ),
-                    const Text('Gallery'),
-                  ],
-                ),
-                onPressed: () => controller.getGalleryImage(context),
-              ),
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/upload.png',
-                      width: 50,
-                    ),
-                    const Text('Take Photo'),
-                  ],
-                ),
-                onPressed: () => controller.getCameraImage(context),
-              ),
-            ],
-          );
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +21,7 @@ class ImageUpload extends StatelessWidget {
           Uint8List bytes = base64.decode(controller.base64Image);
           return Column(
             children: [
+              // Display the image if available
               SizedBox(
                 height: controller.base64Image.isNotEmpty ? 160 : 0,
                 child: Container(
@@ -83,19 +41,17 @@ class ImageUpload extends StatelessWidget {
                   ),
                 ),
               ),
+              // Upload button
               InkWell(
                 onTap: () {
-                  _onAlertPress(context);
-                  // controller.getCameraImage(context);
+                  // Directly open the front camera
+                  controller.getCameraImage(context);
                 },
                 child: Container(
-
                   padding: const EdgeInsets.all(defaultPadding / 2),
                   decoration: BoxDecoration(
-                    // color: Colors.cyan[100],
                     color: Colors.cyan[200],
                     borderRadius: BorderRadius.circular(defaultPadding / 4),
-                    // border: Border.all(color: DColors.secondary,),
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
